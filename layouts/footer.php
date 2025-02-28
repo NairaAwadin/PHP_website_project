6899c65e1b89128d041e8f1adf39ea020b5be490
Naira <naira.awadin@epitech.eu>
2025-02-28 04:46:55 +0100

    [ADD] Navbar + styles avec bootstrap et CSS

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,11 +1,11 @@
 
 
     <footer>
-        <p>FOOTER</p>
+        <p>&copy; 2025 Van Gogh. Vous aimez l'art ? Nous aussi !</p>
     </footer>
 
 
     <script src="../static/js/app.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
